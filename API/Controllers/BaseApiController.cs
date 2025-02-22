8a1782954dbd5616e6699d019a9de7b6824b0e97
karadagserhat <serhatkaradagg@outlook.com>
2025-02-22 17:34:30 +0300

    section 4 - CQRS

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
 namespace API.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
   public class BaseApiController : ControllerBase
   {
+    private IMediator? _mediator;
+
+    protected IMediator Mediator =>
+        _mediator ??= HttpContext.RequestServices.GetService<IMediator>()
+            ?? throw new InvalidOperationException("IMediator service is unavailable");
   }
 }
